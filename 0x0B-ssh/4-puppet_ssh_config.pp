c5564ba964fade878fd9024ff26466322bb33991
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-04 22:06:44 +0000

    create 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 #  using Puppet to make changes to our configuration file.
-file_line {'disable password login':
+file_line { 'disable password login':
     path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no',
 }
-file_line {'pad to add the private key':
-    path => 'etc/ssh/ssh_config',
+file_line { 'pad to add the private key':
+    path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/holberton',
 }
