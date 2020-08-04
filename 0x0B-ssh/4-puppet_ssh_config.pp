528fd17d6dfdf5461855d32ad0448a524a28ed0a
Charliemur2 <charliemurciamunoz@gmail.com>
2020-08-04 21:59:12 +0000

    create 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -4,7 +4,7 @@
 # so that you can connect to a server without typing a password.
 file_line {'disable password login':
     path => '/etc/ssh/sshd_config',
-    line => 'PasswordAuthentication yes',
+    line => 'PasswordAuthentication no',
 }
 file_line {'pad to add the private key':
     path => 'etc/ssh/sshd_config',
