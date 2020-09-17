66d6d439beabf608fde7a4f329607c54054aac6d
Charliemur2 <charliemurciamunoz@gmail.com>
2020-09-17 22:41:34 +0000

    create 1-top_ten.py 1-main.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,10 @@ import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/top.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     req = requests.get(url, headers={'User-agent': 'Carlos'},
                        allow_redirects=False, params={'limit': 10})
-    if req.status_code == 200:
+    if req.json().get('data'):
         for posts in req.json().get('data').get('children'):
             print(posts.get('data').get('title'))
     else:
