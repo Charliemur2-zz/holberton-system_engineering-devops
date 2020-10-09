1a48dc004dd9ec7ea8ae7f7ed4ae25d5f072440d
Charliemur2 <charliemurciamunoz@gmail.com>
2020-10-09 19:27:00 +0000

    create README.md 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@ exec { 'limit':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   command => "sed -i 's/15/1024/' /etc/default/nginx",
 }
-exec { 'restart':
+-> exec { 'restart':
   path    => '/usr/bin/',
   command => 'service nginx restart',
 }
